ef8fb17bfd7436e359995f50594e0508de4cdea4
Xavi Anyo <javiguada60@hotmail.com>
2022-05-22 23:20:31 +0200

    Fix linter

--- app/src/main/java/com/xavi/marvelheroes/FirstFragment.kt
+++ app/src/main/java/com/xavi/marvelheroes/FirstFragment.kt
@@ -1,10 +1,10 @@
 package com.xavi.marvelheroes
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.xavi.marvelheroes.databinding.FragmentFirstBinding
 
@@ -13,32 +13,29 @@ import com.xavi.marvelheroes.databinding.FragmentFirstBinding
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private var binding: FragmentFirstBinding? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
-
+    ): View = binding?.root ?: with(
+        FragmentFirstBinding.inflate(inflater, container, false)
+    ) {
+        binding = this
+        root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding?.buttonFirst?.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
+        binding = null
     }
 }
--- app/src/main/java/com/xavi/marvelheroes/MainActivity.kt
+++ app/src/main/java/com/xavi/marvelheroes/MainActivity.kt
@@ -1,15 +1,15 @@
 package com.xavi.marvelheroes
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.snackbar.Snackbar
 import com.xavi.marvelheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -55,7 +55,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
