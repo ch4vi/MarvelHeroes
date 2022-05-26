c518adb53e848041d49806fde36b28e97591cf96
Xavi Anyo <javiguada60@hotmail.com>
2022-05-27 00:34:32 +0200

    Added pagination tests

--- app/src/test/java/com/xavi/marvelheroes/constants/Utils.kt
+++ app/src/test/java/com/xavi/marvelheroes/constants/Utils.kt
@@ -14,3 +14,28 @@ fun <T> mockFetch(
     every { networkClient.client() } returns client
     every { client.create(apiClass) } returns service
 }
+
+//private suspend fun <T : Any> PagingData<T>.collectDataForTest(): List<T> {
+//    val dcb = object : DifferCallback {
+//        override fun onChanged(position: Int, count: Int) {}
+//        override fun onInserted(position: Int, count: Int) {}
+//        override fun onRemoved(position: Int, count: Int) {}
+//    }
+//    val items = mutableListOf<T>()
+//    val dif = object : PagingDataDiffer<T>(dcb, TestCoroutineDispatcher()) {
+//        override suspend fun presentNewList(
+//            previousList: NullPaddedList<T>,
+//            newList: NullPaddedList<T>,
+//            newCombinedLoadStates: CombinedLoadStates,
+//            lastAccessedIndex: Int,
+//            onListPresentable: () -> Unit
+//        ): Int? {
+//            for (idx in 0 until newList.size)
+//                items.add(newList.getFromStorage(idx))
+//            onListPresentable()
+//            return null
+//        }
+//    }
+//    dif.collectFrom(this)
+//    return items
+//}
