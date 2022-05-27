879b6e5de7800d388e7343412c4c7ba6d84eb69d
Chavi Anyó <ch4vi@users.noreply.github.com>
2022-05-27 15:25:56 +0200

    Show character detail (#3)
    * Updated domain models as Parcelable model
    
    * Prepared detail fragment
    
    * Added detail info with minimum info
    
    * Fix tests

--- app/src/main/java/com/xavi/marvelheroes/domain/model/ThumbnailDomainModel.kt
+++ app/src/main/java/com/xavi/marvelheroes/domain/model/ThumbnailDomainModel.kt
@@ -1,3 +1,7 @@
 package com.xavi.marvelheroes.domain.model
 
-data class ThumbnailDomainModel(val url: String)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ThumbnailDomainModel(val url: String) : Parcelable
